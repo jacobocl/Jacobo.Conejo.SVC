f583e878f555591dccd1875d5c4abb1c498d4f0e
JacoboCL <j.conejol@alumnos.upm.es>
2016-09-22 20:01:16 +0200

    Create class C22Test

---
+++ src/test/java/model/C22Test.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C22Test {
+
+    private C22 c22;
+
+    @Before
+    public void before() {
+        c22 = new C22();
+    }
+
+    @Test
+    public void testMA() {
+        assertEquals("C22:mA()", c22.mA());
+    }
+
+}
